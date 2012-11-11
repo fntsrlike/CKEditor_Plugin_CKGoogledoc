3eb4d09abd54dbdbbcd19ebb2570b0c52d9a3fd1
fntsrlike <fntsrlike@gmail.com>
2012-11-11 23:37:36 +0800

    Update Files

---
+++ plugin/ckgoogledoc/plugin.js
@@ -0,0 +1,71 @@
+(function() {
+
+CKEDITOR.plugins.add('ckgoogledoc',
+{
+  init: function(editor)
+  {
+    /* COMMAND */
+    editor.addCommand('insertGoogledoc', new CKEDITOR.dialogCommand('ckgoogledocDialog'));
+
+    /* BUTTON */
+    editor.ui.addButton('CKGoogledoc',
+    {
+      label: 'Google docs',
+      command: 'insertGoogledoc',
+      icon: this.path + 'ckgoogledoc.png'
+    });
+
+    /* DIALOG */
+    CKEDITOR.dialog.add('ckgoogledocDialog', function (editor)
+    {
+      return {
+        title : 'Embedd Google Docs iFrame',
+        minWidth : 300,
+        minHeight : 200,
+        contents :
+        [{
+          id : 'tab1',
+          label : 'Settings',
+          elements :
+          [{
+            type : 'text',
+            id : 'url',
+            label : 'Document URL',
+            validate : CKEDITOR.dialog.validate.notEmpty("Document URL should be provided and has to be a well formated URL")
+          },
+          {
+            type : 'text',
+            id : 'width',
+            label : 'Document Width in pixels',
+            onShow : function() { this.setValue('600'); },
+            validate : CKEDITOR.dialog.validate.notEmpty("Document width should be provided")
+          },
+          {
+            type : 'text',
+            id : 'height',
+            label : 'Document Height in pixels',
+            onShow : function() { this.setValue('780'); },
+            validate : CKEDITOR.dialog.validate.notEmpty("Document height should be provided")
+          }]
+        }],
+        onOk : function()
+          {
+            var dialog = this;
+            var url = dialog.getValueOf('tab1', 'url');
+            var width = dialog.getValueOf('tab1', 'width');
+            var height = dialog.getValueOf('tab1', 'height');
+
+            var id = url.substring(32)
+            
+            var inplaceTag = '<iframe src="https://docs.google.com/file/d/' + encodeURIComponent(id) + '/preview" width="' + parseInt(width) + '" height="' + parseInt(height) + '" style="border: none;">';
+
+            editor.insertHtml(inplaceTag);
+          }
+        };
+//dialog.add
+      });
+//init:
+    }
+//plugin.add
+  });
+})();
